a3b5d32b233a01ab14f36b5866b878777eeeeb7c
Colin Finger <finger.colin@gmail.com>
2019-06-21 10:20:23 +0200

    revert changes

--- server.js
+++ server.js
@@ -10,29 +10,25 @@ const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
 
+const db = knex({
+  client: 'pg',
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  }
+});
 
+/* 
 
 const db = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
-    user : 'aneagoie',
+    user : 'postgres',
     password : '',
-    database : 'smart-brain'
+    database : 'test'
   }
 });
-
-
-/* 
-
-const db = knex({
-	client: 'pg',
-	connection: {
-		connectionString: process.env.DATABASE_URL,
-		ssl: true,
-	}
-});
-
 */
 
 
@@ -55,3 +51,33 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.listen(process.env.PORT || 3001, () => {
 	console.log('app is running');
 })
+
+
+
+/* {
+/// Package.json
+
+{
+  "name": "projektkacper-api",
+  "version": "1.0.0",
+  "description": "",
+  "main": "server.js",
+  "scripts": {
+    "start": "nodemon"
+  },
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "bcrypt-nodejs": "0.0.3",
+    "body-parser": "^1.19.0",
+    "cors": "^2.8.5",
+    "express": "^4.17.1",
+    "knex": "^0.17.6",
+    "pg": "^7.11.0"
+  },
+  "devDependencies": {
+    "nodemon": "^1.19.1"
+  }
+}
+
+ */
